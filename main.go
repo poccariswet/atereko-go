ea39c2c3614222eac7978c2a02be828bf5cfa4e8
poccariswet <s1240231@gmail.com>
2020-04-15 23:11:08 +0900

    [ADD] sample format ffmpeg

--- main.go
+++ main.go
@@ -1,6 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os/exec"
+)
+
+type ffmpeg struct {
+	*exec.Cmd
+}
+
+func (f *ffmpeg) setArgs(args ...string) {
+	f.Args = append(f.Args, args...)
+}
+
+func (f *ffmpeg) setDir(dir string) {
+	f.Dir = dir
+}
+
+func (f *ffmpeg) run(output string) error {
+	f.setArgs(output)
+	return f.Run()
+}
+
+func newffmpeg(ctx context.Context) (*ffmpeg, error) {
+	cmdPath, err := exec.LookPath("ffmpeg")
+	if err != nil {
+		return nil, err
+	}
+
+	return &ffmpeg{exec.CommandContext(
+		ctx,
+		cmdPath,
+	)}, nil
+}
 
 func format_hhmmss(target_seconds int) string {
 	target_minutes := target_seconds / 60
@@ -12,5 +45,22 @@ func format_hhmmss(target_seconds int) string {
 }
 
 func main() {
-	fmt.Println(format_hhmmss(90))
+	ctx := context.Background()
+
+	inputTime := format_hhmmss(10)
+	output := "sample.mp4"
+
+	f, err := newffmpeg(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	f.setArgs(
+		"-i", "sample/girl_shout.mp4",
+		"-itsoffset", inputTime, "-i",
+		"sample/shout.mp3", "-c:v",
+		"copy", "-map", "0:v:0", "-map", "1:a:0",
+	)
+
+	f.run(output)
 }
