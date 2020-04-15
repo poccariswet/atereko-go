b469bea4807560a928ba5b0edaeb9cae210a0aa3
poccariswet <s1240231@gmail.com>
2020-04-15 22:45:56 +0900

    [ADD] format

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+func format_hhmmss(target_seconds int) string {
+	target_minutes := target_seconds / 60
+	seconds := target_seconds % 60
+	hour := target_minutes / 60
+	minutes := target_minutes % 60
+
+	return fmt.Sprintf("%02d:%02d:%02d", hour, minutes, seconds)
+}
+
 func main() {
-	fmt.Println("aaa")
+	fmt.Println(format_hhmmss(90))
 }
